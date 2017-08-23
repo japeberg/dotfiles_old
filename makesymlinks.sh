d0fc79ee6bfa6960c23650347b2e53d6e3372e5a
Jan-Peter Bergmann <jan-peter.bergmann@gmail.com>
2017-08-23 20:56:01 +0200

    Von Pathogen auf Vundle umgestellt. Typescript-vim, YouCompleteMe und syntastic hinzugefügt. Installskript um Vundle und YCM erweitert.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -28,4 +28,20 @@ for file in $files; do
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
+
+
+echo "Installing Vundle for vim"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "...done"
+
+echo "Compiling YouCompleteMe"
+sudo apt-get install build-essential cmake
+sudo apt-get install python-dev python3-dev
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
+echo "...done"
+
+echo "Installing VIM-Plugins with Vundle"
+vim +PluginInstall +qall
+echo "...done"
 done
