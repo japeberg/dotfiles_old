04f631bac39bbd98636f435afff5e65fae0f7d78
Jan-Peter Bergmann <janpeter.bergmann@gmail.com>
2019-02-04 16:00:15 +0100

    Fish-install improved

--- install-fish.sh
+++ install-fish.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 ############################
 
+
+echo "Install fish 3.x repository"
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install fish
 
+echo "Change default shell to fish"
+chsh -s `which fish`
+
+echo "Check if version 3.x is installed"
 fish -v
